f9e7f252888b791d9a418717b0f639e2edf83b38
Daniel Nunes <132585776+Maxjoni@users.noreply.github.com>
2024-03-22 22:57:40 +0000

    Update login.dart
    Wrapping up the column with a scrollbar in case smaller screens are used and the layout might not entirely fit on-screen (especially with the validation warnings).

--- lib/screens/initial_area/login.dart
+++ lib/screens/initial_area/login.dart
@@ -18,180 +18,182 @@ class LoginScreenState extends State<LoginScreen> {
     
     return Scaffold(
       backgroundColor: isDarkTheme ? Colors.black : Colors.white,
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextButton(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isDarkTheme ? Colors.black : Colors.white,
+                  ),
+                  child: Icon(
+                    isDarkTheme ? Icons.light_mode : Icons.dark_mode,
+                    color: isDarkTheme ? Colors.white : Colors.black,
+                    size: 25,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Sorttasks',
+                  style: TextStyle(
+                    color: isDarkTheme ? Colors.white : Colors.black,
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.task,
+                    color: isDarkTheme ? Colors.white : Colors.black,
+                    size: 40,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 70),
+            Text(
+              'Welcome!',
+              style: TextStyle(
+                color: isDarkTheme ? Colors.white : Colors.black,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Please sign in to continue',
+              style: TextStyle(
+                color: isDarkTheme ? Colors.white : Colors.black,
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 30),
+            LoginForm(
+              onEmailChanged: (email) {
+                // No need to manage email state here
+              },
+              onPasswordChanged: (password) {
+                // No need to manage password state here
+              },
+            ),
+            const SizedBox(height: 50),
+            Text(
+              'Forgot your password?',
+              style: TextStyle(
+                fontSize: 14,
+                color: isDarkTheme ? Colors.white : Colors.black,
+              )
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: 190,
+              height: 50,
+              child: ElevatedButton(
                 onPressed: () {
-                  setState(() {
-                    Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
-                  });
+                  // Recover password logic
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: isDarkTheme ? Colors.black : Colors.white,
+                  backgroundColor: isDarkTheme ? const Color.fromRGBO(255, 80, 80, 0.8) : const Color.fromRGBO(255, 198, 198, 1.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                 ),
-                child: Icon(
-                  isDarkTheme ? Icons.light_mode : Icons.dark_mode,
-                  color: isDarkTheme ? Colors.white : Colors.black,
-                  size: 25,
-                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Recover it here',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: isDarkTheme ? Colors.white : Colors.black,
+                      )
+                    ),
+                    const SizedBox(width: 5),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.help_center,
+                        color: isDarkTheme ? Colors.white : Colors.black,
+                        size: 40,
+                      ),
+                    ),
+                  ],
+                )                
               ),
             ),
-          ),
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Sorttasks',
-                style: TextStyle(
-                  color: isDarkTheme ? Colors.white : Colors.black,
-                  fontSize: 24,
-                ),
-              ),
-              const SizedBox(width: 5),
-              Container(
-                width: 50,
-                height: 50,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(
-                  Icons.task,
-                  color: isDarkTheme ? Colors.white : Colors.black,
-                  size: 40,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 70),
-          Text(
-            'Welcome!',
-            style: TextStyle(
-              color: isDarkTheme ? Colors.white : Colors.black,
-              fontSize: 24,
+            const SizedBox(height: 50),
+            Text(
+              'No account? Register here:',
+              style: TextStyle(
+                fontSize: 14,
+                color: isDarkTheme ? Colors.white : Colors.black,
+              )
             ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Please sign in to continue',
-            style: TextStyle(
-              color: isDarkTheme ? Colors.white : Colors.black,
-              fontSize: 14,
-            ),
-          ),
-          const SizedBox(height: 30),
-          LoginForm(
-            onEmailChanged: (email) {
-              // No need to manage email state here
-            },
-            onPasswordChanged: (password) {
-              // No need to manage password state here
-            },
-          ),
-          const SizedBox(height: 50),
-          Text(
-            'Forgot your password?',
-            style: TextStyle(
-              fontSize: 14,
-              color: isDarkTheme ? Colors.white : Colors.black,
+            const SizedBox(height: 10),
+            SizedBox(
+              width: 300,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Go to register page logic
+                  Navigator.pushReplacementNamed(context, '/register');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isDarkTheme ? const Color.fromRGBO(255, 155, 63, 0.8) : const Color.fromRGBO(255, 155, 63, 1.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'CREATE ACCOUNT',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: isDarkTheme ? Colors.white : Colors.black,
+                      )
+                    ),
+                    const SizedBox(width: 10),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.app_registration,
+                        color: isDarkTheme ? Colors.white : Colors.black,
+                        size: 40,
+                      ),
+                    ),
+                  ],
+                )                
+              ),
             )
-          ),
-          const SizedBox(height: 10),
-          SizedBox(
-            width: 190,
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {
-                // Recover password logic
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: isDarkTheme ? const Color.fromRGBO(255, 80, 80, 0.8) : const Color.fromRGBO(255, 198, 198, 1.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Recover it here',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: isDarkTheme ? Colors.white : Colors.black,
-                    )
-                  ),
-                  const SizedBox(width: 5),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      Icons.help_center,
-                      color: isDarkTheme ? Colors.white : Colors.black,
-                      size: 40,
-                    ),
-                  ),
-                ],
-              )                
-            ),
-          ),
-          const SizedBox(height: 50),
-          Text(
-            'No account? Register here:',
-            style: TextStyle(
-              fontSize: 14,
-              color: isDarkTheme ? Colors.white : Colors.black,
-            )
-          ),
-          const SizedBox(height: 10),
-          SizedBox(
-            width: 300,
-            height: 60,
-            child: ElevatedButton(
-              onPressed: () {
-                // Go to register page logic
-                Navigator.pushReplacementNamed(context, '/register');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: isDarkTheme ? const Color.fromRGBO(255, 155, 63, 0.8) : const Color.fromRGBO(255, 155, 63, 1.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'CREATE ACCOUNT',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: isDarkTheme ? Colors.white : Colors.black,
-                    )
-                  ),
-                  const SizedBox(width: 10),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      Icons.app_registration,
-                      color: isDarkTheme ? Colors.white : Colors.black,
-                      size: 40,
-                    ),
-                  ),
-                ],
-              )                
-            ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -347,5 +349,4 @@ class LoginFormState extends State<LoginForm> {
       ],
     );
   }
-
 }
