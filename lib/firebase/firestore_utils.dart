5d56d835caed1a1ccbdb3c221b82a04218f84c1d
Daniel Nunes <132585776+Maxjoni@users.noreply.github.com>
2024-03-25 14:39:32 +0000

    Update firestore_utils.dart

---
+++ lib/firebase/firestore_utils.dart
@@ -0,0 +1,309 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:sorttasks/main.dart';
+
+class FirestoreUtils {
+
+  // 'USERS' COLLECTION IN FIRESTORE
+
+  static Future<bool> findUserDocumentId(String userUid) async {
+    try {
+      // Reference to the 'users' collection
+      CollectionReference usersCollection = FirebaseFirestore.instance.collection('users');
+
+      // Check if the userUid matches any document ID
+      DocumentSnapshot documentSnapshot = await usersCollection.doc(userUid).get();
+
+      // Return true if a matching document is found
+      return documentSnapshot.exists;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  static Future<Map<String, String>?> getUserData() async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        // Reference to the 'users' collection and the specific document
+        DocumentReference userRef = FirebaseFirestore.instance.collection('users').doc(currentUser.uid);
+
+        // Fetch the document snapshot
+        DocumentSnapshot snapshot = await userRef.get();
+
+        if (snapshot.exists) {
+          // Access the user data from the snapshot
+          Map<String, dynamic> userData = snapshot.data() as Map<String, dynamic>;
+          String firstName = userData['firstname'];
+          String lastName = userData['lastname'];
+
+          return {'first_name': firstName, 'last_name': lastName};
+        }
+      }
+
+      return null;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error getting authenticated user data: $e');
+      }
+      return null;
+    }
+  }
+
+  // USER UPDATE
+
+  static Future<void> updateUserPassword(BuildContext context, String newPassword) async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        await currentUser.updatePassword(newPassword);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error updating user password: $e');
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error updating password. Please try again.'),
+        ),
+      );
+      rethrow;
+    }
+  }
+
+  static Future<void> updateUser(BuildContext context, String newUsername, String newEmail, String newPassword, String currentEmail) async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        // Check if there are changes in email
+        bool isEmailChanged = newEmail != currentEmail;
+
+        // Update Firestore document with new username
+        await updateUserDetails(context, newUsername);
+        // Update authenticated user's password
+        await currentUser.updatePassword(newPassword);
+
+        // Send verification email if email is changed
+        if (isEmailChanged) {
+          // Update authenticated user's email
+          await currentUser.verifyBeforeUpdateEmail(newEmail);
+          await currentUser.sendEmailVerification();
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Email updated successfully. Please verify your new email address when possible. While you dont, you will need to use your old email.'),
+              duration: Duration(seconds: 10),
+            ),
+          );
+        }
+        
+        // Log out the user after email update to force reauthentication
+        await FirebaseAuth.instance.signOut();
+        SorttasksApp.setLoggedInUser(null);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error updating user email and password: $e');
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error updating email and password. Please try again.'),
+          duration: Duration(seconds: 5),
+        ),
+      );
+      rethrow;
+    }
+  }
+
+  static Future<void> updateUserDetails(BuildContext context, String newUsername) async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        // Fetch the current user data from Firestore
+        DocumentSnapshot snapshot = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+        Map<String, dynamic> currentUserData = snapshot.data() as Map<String, dynamic>;
+
+        // Check if there are changes in username
+        bool isUsernameChanged = currentUserData['username'] != newUsername;
+
+        // Update Firestore document with new username
+        await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).update({
+          'username': newUsername,
+        });
+
+        // Show a Snackbar if username changed
+        if (isUsernameChanged) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('User details updated successfully. Please re-enter your credentials to login.'),
+              duration: Duration(seconds: 10),
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error updating user details: $e');
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error updating user details. Please try again.'),
+        ),
+      );
+      rethrow;
+    }
+  }
+
+  // USER CREATE AND DELETE
+
+  static Future<void> addUser(String username, String email, String password) async {
+    try {
+      // Create a new user in Firebase Authentication
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Get the ID of the newly created user
+      String userId = userCredential.user!.uid;
+
+      // Add user details to Firestore
+      await FirebaseFirestore.instance.collection('users').doc(userId).set({
+        'username': username,
+      });
+
+      // Send verification email
+      await userCredential.user!.sendEmailVerification();
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error adding user to Firestore: $e');
+      }
+      rethrow;
+    }
+  }
+
+  static Future<void> deleteUser(String userId) async {
+    try {
+      // Delete the user's document from the 'users' collection
+      await FirebaseFirestore.instance.collection('users').doc(userId).delete();
+
+      // Delete all events owned by the user from the 'events' collection
+      QuerySnapshot eventsSnapshot = await FirebaseFirestore.instance.collection('events').where('userOwner', isEqualTo: userId).get();
+      for (QueryDocumentSnapshot eventSnapshot in eventsSnapshot.docs) {
+        await eventSnapshot.reference.delete();
+      }
+
+      // Delete the user from authentication
+      await FirebaseAuth.instance.currentUser?.delete();
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error deleting user: $e');
+      }
+      rethrow;
+    }
+  }
+
+  // USER AUTHENTICATION AND VERIFICATION
+
+  static Future<bool> login(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      SorttasksApp.setLoggedInUser(userCredential.user);
+
+      // Login was a success
+      return true;
+    } catch (e) {
+      // Authentication failure
+      if (kDebugMode) {
+        print('Authentication failed: $e');
+      }
+      return false;
+    }
+  }
+
+  static Future<String?> getLoggedInUserId() async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+      return currentUser?.uid;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error getting logged-in user ID: $e');
+      }
+      return null;
+    }
+  }
+
+  static Future<bool> verifyPassword(String password) async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        // Use reauthentication to verify the password
+        AuthCredential credential = EmailAuthProvider.credential(email: currentUser.email!, password: password);
+        await currentUser.reauthenticateWithCredential(credential);
+        return true;
+      }
+
+      return false;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error verifying password: $e');
+      }
+      return false;
+    }
+  }
+
+  static Future<bool> checkEmailVerification() async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        await currentUser.reload(); // Refresh user data
+
+        // Check if the email is verified
+        if (currentUser.emailVerified) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error checking email verification: $e');
+      }
+      return false;
+    }
+  }
+
+  static Future<bool> checkEmailAvailable(String email) async {
+    try {
+      // Reference to FirebaseAuth instance
+      FirebaseAuth auth = FirebaseAuth.instance;
+
+      // Fetch sign-in methods for the provided email
+      List<String> signInMethods = await auth.fetchSignInMethodsForEmail(email);
+
+      // Return true if the list is empty (email is available)
+      return signInMethods.isEmpty;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error checking email availability: $e');
+      }
+      return false;
+    }
+  }
+
+}
